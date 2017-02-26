cf1ae0255004bb7efbbd247901172c2783c7a5b4
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-02-26 23:00:07 +0100

    Fix fastboot compatibility

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,6 +29,5 @@ module.exports = function (defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   return app.toTree();
 };
