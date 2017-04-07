7e25e1f788f0b14a52c7747ac93ef09688850231
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-04-07 19:54:14 +0200

    Upgrade packages

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -3,16 +3,13 @@ import Application from '../../app';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  let application;
-
   // use defaults, but you can override
   let attributes = Ember.assign({}, config.APP, attrs);
 
-  Ember.run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }
