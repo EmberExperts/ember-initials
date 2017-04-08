7dc4901de4434a85644da37c6a61bc72c3858f7a
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-04-08 16:44:54 +0200

    Update packages (#27)
    * Upgrade packages
    
    * Fix eslint
    
    * Update packages
    
    * Remove yarn

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    browser: true
+  },
+  rules: {
+  },
+  globals: {
+    md5: true,
+    define: true,
+    FastBoot: true
+  }
+};
