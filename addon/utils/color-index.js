fea553748a2097a6a2919ae95e4694f0f6c4f590
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-05-01 16:32:58 +0200

    Fix hashCode

--- addon/utils/color-index.js
+++ addon/utils/color-index.js
@@ -8,13 +8,13 @@ export default function (seedText, colorsLength) {
 function hashCode(string) {
   let hash = 0;
 
-  if (string.length === 0) return hash;
-
-  for (let i = 0; i < string.length; i++) {
-    let char = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
-    hash |= 0; // Convert to 32bit integer
+  if (string && string.length > 0) {
+    for (let i = 0; i < string.length; i++) {
+      let char = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash |= 0; // Convert to 32bit integer
+    }
   }
 
-  return hash;
+  return hash & hash;
 }
--- tests/unit/utils/color-index-test.js
+++ tests/unit/utils/color-index-test.js
@@ -7,3 +7,7 @@ test('color index', function(assert) {
   let text = 'Ember Initials'
   assert.equal(ColorIndex(text, 27), 6);
 });
+
+test('when text is null', function(assert) {
+  assert.equal(ColorIndex(null, 27), 0);
+});
