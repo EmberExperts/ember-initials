ef0dae7f6293ffe90f8023bc1238952f44d3b3a5
Ryan Quinn <ryan@mazondo.com>
2017-12-29 16:27:34 -0700

    remove object-hash, tests passing

--- addon/utils/store.js
+++ addon/utils/store.js
@@ -1,8 +1,8 @@
 import { computed } from '@ember/object';
 import EmberObject from "@ember/object";
-import hash from 'object-hash';
+import md5 from 'md5';
 import SvgGenerator from '../utils/generators/svg';
-
+import { typeOf } from '@ember/utils';
 export default EmberObject.extend({
   cache: null,
 
@@ -21,11 +21,26 @@ export default EmberObject.extend({
   },
 
   initialsFor(properties) {
-    let key = hash(properties);
+    let hashableProp = this._ensureHashable(properties);
+    let key = md5(hashableProp);
     return this.get('cache')[key] || this._create(key, properties);
   },
 
   _create(key, properties) {
     return this.get('cache')[key] = this.get('generator').generate(properties);
+  },
+
+  _ensureHashable(properties) {
+    if (typeOf(properties) !== 'object') {
+      // this should never happen, but just in case
+      return properties.toString();
+    }
+
+    let pKeys = Object.keys(properties).sort();
+    let result = [];
+    pKeys.forEach((key) => {
+      result.concat([key, properties[key]]);
+    });
+    return result.toString();
   }
 });
--- index.js
+++ index.js
@@ -10,20 +10,12 @@ module.exports = {
 
   included: function(app) {
     this._super.included(app);
-
     app.import('vendor/ember-initials/md5.js');
     app.import('vendor/shims/md5.js', { type: 'vendor' });
-
-    app.import('vendor/ember-initials/object_hash.js');
-    app.import('vendor/shims/object-hash.js', { type: 'vendor' });
   },
 
   md5Path() {
     return path.join(this.app.project.nodeModulesPath, 'blueimp-md5', 'js');
-  },
-
-  objectHashPath() {
-    return path.join(this.app.project.nodeModulesPath, 'object-hash', 'dist');
   },
 
   treeForVendor(tree) {
@@ -32,11 +24,6 @@ module.exports = {
     trees.push(new Funnel(this.md5Path(), {
       destDir: 'ember-initials',
       files: ['md5.js']
-    }));
-
-    trees.push(new Funnel(this.objectHashPath(), {
-      destDir: 'ember-initials',
-      files: ['object_hash.js']
     }));
 
     return mergeTrees(trees);
