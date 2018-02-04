b3eb8c28c78a63e33f2b08e985db60fa9ccbc6f5
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-02-04 23:39:38 +0100

    Remove object hash

--- addon/utils/hash.js
+++ addon/utils/hash.js
@@ -1,5 +1,15 @@
-import objectHash from 'object-hash';
+import md5 from 'md5';
 
 export default function hash(object = {}) {
-  return objectHash(object);
+  return md5(serialize(object));
+}
+
+function serialize(obj) {
+  if (Array.isArray(obj)) {
+    return JSON.stringify(obj.map(i => serialize(i)))
+  } else if (typeof obj === 'object' && obj !== null) {
+    return Object.keys(obj).sort().map(k => `${k}:${serialize(obj[k])}`).join('|')
+  }
+
+  return obj
 }
