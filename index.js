caf36b821715f18463e3acf013c690318dd8c700
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-02-05 09:15:19 +0100

    Remove object hash (#52)

--- index.js
+++ index.js
@@ -12,17 +12,10 @@ module.exports = {
 
     app.import('vendor/ember-initials/md5.js');
     app.import('vendor/shims/md5.js', { type: 'vendor' });
-
-    app.import('vendor/ember-initials/object_hash.js');
-    app.import('vendor/shims/object-hash.js', { type: 'vendor' });
   },
 
   md5Path() {
     return path.join(this.app.project.nodeModulesPath, 'blueimp-md5', 'js');
-  },
-
-  objectHashPath() {
-    return path.join(this.app.project.nodeModulesPath, 'object-hash', 'dist');
   },
 
   treeForVendor(tree) {
@@ -31,11 +24,6 @@ module.exports = {
     trees.push(new Funnel(this.md5Path(), {
       destDir: 'ember-initials',
       files: ['md5.js']
-    }));
-
-    trees.push(new Funnel(this.objectHashPath(), {
-      destDir: 'ember-initials',
-      files: ['object_hash.js']
     }));
 
     return mergeTrees(trees);
