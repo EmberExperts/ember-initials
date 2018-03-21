70bc26e3f4074bef417dee40e5be027fd0143ab5
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-03-21 09:09:54 +0100

    Fix md5 (#57)

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 
 module.exports = {
   name: 'ember-initials',
+
+  included: function(app) {
+    this._super.included(app);
+    app.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
+  }
 }
