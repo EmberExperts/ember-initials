193aa2a60a14e0a39b1bde8fd15236b5ef171d66
Marcelo Mira <hello@marcemira.com>
2018-05-16 10:50:29 -0300

    [FIX] Support for engine add-on
    `app.import` wasn't targeting my engine's dummy app, so this should provide wider support.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 module.exports = {
   name: 'ember-initials',
 
-  included: function(app) {
-    this._super.included(app);
-    app.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
+  included: function() {
+    this._super.included.apply(this, arguments);
+    let target = this._findHost();
+    target.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
   }
 }
