db42ca0c781e6da5b1278fefafd04862d93b5991
Marcelo Mira <hello@marcemira.com>
2018-05-17 11:22:15 -0300

    [FIX] Support for engine add-on (#63)
    * [FIX] Support for engine add-on
    
    `app.import` wasn't targeting my engine's dummy app, so this should provide wider support.
    
    * Removing private method
    
    Using `this.import`, removing private `_findHost`

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 module.exports = {
   name: 'ember-initials',
 
-  included: function(app) {
-    this._super.included(app);
-    app.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
+  included: function() {
+    this._super.included.apply(this, arguments);
+    this.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
   }
 }
