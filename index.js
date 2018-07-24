166298e49fa90c0a5a277f6d534eed8c8beecb7d
Ryan Cook <ryanpatrickcook@gmail.com>
2018-07-24 18:22:31 -0400

    port fix for engine

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-initials'
+  name: 'ember-initials',
+
+  included: function() {
+    this._super.included.apply(this, arguments);
+    let target = this._findHost();
+    this.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
+  }
 }
