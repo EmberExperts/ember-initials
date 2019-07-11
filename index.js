2cf15a39656fe3c92db5f1c3828a4f1bea5ead71
danwenzel <dan@danwenzel.com>
2019-07-11 17:58:10 -0500

    Remove blueimp-md5

--- index.js
+++ index.js
@@ -2,11 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-initials',
-
-  included: function() {
-    this._super.included.apply(this, arguments);
-    let target = this._findHost();
-    this.import('node_modules/blueimp-md5/js/md5.min.js', { using: [{ transformation: 'amd', as: 'md5' }]});
-  }
+  name: 'ember-initials'
 }
