015074ed76dfdfadb504a3a544e567160ca903dc
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-12-28 21:05:32 +0100

    Better generator

--- addon/utils/generators/svg.js
+++ addon/utils/generators/svg.js
@@ -1,38 +1,30 @@
 export default class Svg {
   generate(properties) {
-    let textElement = this._generateTextElement(properties.initials, properties.initialsColor, properties.textStyles);
-    return this._generateSvgElement(textElement, properties.backgroundStyles);
-  }
-
-  // Private
-
-  _generateTextElement(text, color, styles = {}) {
-    return this._generateElement('text', text, styles, {
+    const textElement = this._generateElement('text', properties.initials, {
+      style: this._stringify(properties.textStyles, (key, value) => `${key}: ${value};`),
       y: '50%',
       x: '50%',
       dy: '0.35em',
       'text-anchor': 'middle',
       'pointer-events': 'none',
-      fill: color,
+      fill: properties.initialsColor,
     });
-  }
 
-  _generateSvgElement(text, styles = {}) {
-    return this._generateElement('svg', text, styles, {
+    return this._generateElement('svg', textElement, {
+      style: this._stringify(properties.backgroundStyles, (key, value) => `${key}: ${value};`),
       xmlns: 'http://www.w3.org/2000/svg',
       'pointer-events': 'none',
       'viewBox': '0 0 100 100'
     });
   }
 
-  _generateElement(name, content = '', styles = {}, attrs = {}) {
-    let attrsString = this._transformObject(attrs, (key) => `${key}="${attrs[key]}"`)
-    let stylesString = this._transformObject(styles, (key) => `${key}: ${styles[key]};`)
+  _generateElement(name, content, attrs = {}) {
+    const attrsString = this._stringify(attrs, (key, value) => `${key}="${value}"`);
 
-    return `<${name} ${attrsString} style="${stylesString}">${content}</${name}>`
+    return `<${name} ${attrsString}>${content}</${name}>`
   }
 
-  _transformObject(object, transform = function() {}) {
-    return Object.keys(object).map((key) => transform(key)).join(' ');
+  _stringify(object, transform = function() {}) {
+    return Object.entries(object).map(([key, value]) => transform(key, value)).join(' ');
   }
 }
