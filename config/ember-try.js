665748f624ef8675f23ac8a6b48e7ac2c07ecaf9
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-03-29 11:05:02 +0200

    Updates to 3.17

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,25 +1,24 @@
+/* eslint-disable max-lines-per-function */
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
-    npmOptions: ['> /dev/null'],
-
     scenarios: [
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
       {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
             'ember-source': '~3.12.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0'
           }
         }
       },
